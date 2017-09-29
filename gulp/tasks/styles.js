0181761022d236139f9e9281684c9a5b42380993
Rafa <rcevallos@outlook.com>
2017-09-28 23:56:09 -0500

    Completed large hero block and intro content block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var simpleVars = require('postcss-simple-vars');
+var mixins = require('postcss-mixins');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('styles', function () {
+	return gulp.src('./app/assets/styles/styles.css')
+				.pipe(postcss([cssImport, mixins, simpleVars, autoprefixer, nested]))
+				.on('error', function (error) {
+					console.log(error.toString());
+					this.emit('end');
+				})
+				.pipe(gulp.dest('./app/temp/styles'));
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,2 @@
-// Require dependencies
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-
-// Define tasks
-gulp.task('html', function () {
-	console.log('html changed');
-});
-
-gulp.task('styles', function () {
-	console.log('Imagine Sass or PostCSS tasks running here.')
-})
-
-// Watch files
-gulp.task('watch', function () {
-	
-	watch('./app/index.html', function () {
-		gulp.start('html');
-	});
-
-	watch('./app/assets/styles/**/*.css', function () {
-		gulp.start('styles');
-	});
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
