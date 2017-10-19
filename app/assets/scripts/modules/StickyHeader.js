a2f9186863999811e97fded5826c7c9aebf04f5e
Rafa <rcevallos@outlook.com>
2017-10-18 23:29:33 -0500

    Completed lazy load of images

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -5,13 +5,21 @@ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoi
 class StickyHeader {
 
 	constructor () {
+		this.lazyImages = $('.lazyload');
 		this.siteHeader = $('.site-header');
 		this.headerTriggerElement = $('.large-hero__title');
-		this.createHeaderWaypoint();
 		this.pageSections = $('.page-section');
 		this.headerLinks = $('.primary-nav a');
+		this.createHeaderWaypoint();
 		this.createPageSectionWaypoints();
 		this.addSmoothScrolling();
+		this.refreshWaypoints();
+	}
+
+	refreshWaypoints () {
+		this.lazyImages.on('load', function () {
+			Waypoint.refreshAll();
+		});
 	}
 
 	addSmoothScrolling () {
